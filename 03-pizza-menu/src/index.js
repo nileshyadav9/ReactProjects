eeff0118adefc1a1fc343ae73e9be4fb56f8bd5c
nileshyadav9 <32081380+nileshyadav9@users.noreply.github.com>
2024-05-19 14:58:31 -0400

    React fragments
    addition of React Fragments
    updating props to use de-structured objects for better handling of params
    updating site to gray out the sold out items

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -74,49 +74,72 @@ function Menu() {
       <h2>Menu</h2>
 
       {pizzaList.length > 0 ? (
-        <ul className="pizzas">
-          {pizzaList.map((pizza) => (
-            <Pizza pizzData={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        /* <> creates fragment when more than one root element needs to be returned */
+        /* when fragment needs a key we use <React.Fragment key={}>*/
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from, All
+            from our stone oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzaList.map((pizza) => (
+              <Pizza pizzData={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our Menu, please come back later!</p>
       )}
     </main>
   );
 }
-
-function Pizza(props) {
+// instead of props now destructuring the props into new object
+// make sure to match the object to the props passed to the tag
+function Pizza({ pizzData }) {
   //console.log(props);
-  let style = { disabled: props.pizzData.soldOut };
+  let style = { disabled: pizzData.soldOut };
   return (
-    <li className="pizza" style={style}>
-      <img src={props.pizzData.photoName} alt={props.pizzData.name} />
+    <li
+      className={`pizza  ${pizzData.soldOut ? "sold-out" : ""} `}
+      style={style}
+    >
+      <img src={pizzData.photoName} alt={pizzData.name} />
       <div>
-        <h3>{props.pizzData.name}</h3>
-        <p>{props.pizzData.ingredients}</p>
-        <span>{props.pizzData.price}</span>
-        <span>{props.pizzData.soldOut ? "Sold Out" : ""}</span>
+        <h3>{pizzData.name}</h3>
+        <p>{pizzData.ingredients}</p>
+        {/* <span>{pizzData.price}</span> */}
+        <span>{pizzData.soldOut ? "Sold Out" : pizzData.price}</span>
       </div>
     </li>
   );
 }
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>{props.message}</p>
+      <button className="btn"> Order Now</button>
+    </div>
+  );
+}
+
 function Footer() {
   let openHour = 12;
   let closeHour = 22;
   let curretnHour = new Date().getHours();
   console.log(curretnHour);
   let isOpen = curretnHour > openHour && curretnHour < closeHour;
-  let storeOpenstring = isOpen
-    ? `We're Open until ${closeHour}:00. Come visit us or order Online :)`
-    : `Sorry, We're Closed! We are open between ${openHour} and ${closeHour}`;
+  let storeOpenstring = `We're Open until ${closeHour}:00. Come visit us or order Online :)`;
 
   return (
     <footer className="footer">
-      <div className="order">
-        <p>{storeOpenstring}</p>
-        <button className="btn"> Order Now</button>
-      </div>
+      {isOpen ? (
+        <Order message={storeOpenstring} />
+      ) : (
+        <p>
+          Sorry, We're Closed! We are open between {openHour} and {closeHour}
+        </p>
+      )}
     </footer>
   );
 }
