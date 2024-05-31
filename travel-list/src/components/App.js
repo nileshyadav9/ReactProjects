d2d2a5926fd9cc766de4ff8eebe040ac4a9726ad
nileshyadav9 <32081380+nileshyadav9@users.noreply.github.com>
2024-05-31 18:52:31 -0400

    usePopcorn app initial check-in
    small app to fetch movies from an API and add it our watch list and add ratings
    initial check-in with static data and components

--- travel-list/src/components/App.js
+++ travel-list/src/components/App.js
@@ -1,11 +1,11 @@
 import FlashCards from "./FlashCards";
 import ToDoPackingList from "./ToDoPackingList";
 
-const initialItems = [
+/* const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 2, packed: true },
-];
+]; */
 
 export default function App() {
   return (
