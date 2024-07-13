a30d630ddda35681122725a275660506237fccd1
nileshyadav9 <32081380+nileshyadav9@users.noreply.github.com>
2024-07-13 13:11:29 -0400

    Update Calculator.js
    Update calculator.js file to update the duration based on the control buttons for duration
    Used effect to update the duration and passed all the reactive states as dependency
    Made sure that the duration does not go in negative

--- workout-timer/src/Calculator.js
+++ workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, memo, useEffect } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -6,8 +6,15 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
+  );
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -17,6 +24,12 @@ function Calculator({ workouts, allowSound }) {
     sound.play();
   };
 
+  function handleDurationInc() {
+    setDuration((duration) => Math.floor(duration) + 1);
+  }
+  function handleDurationDec() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+  }
   return (
     <>
       <form>
@@ -66,13 +79,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDurationDec}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleDurationInc}>+</button>
       </section>
     </>
   );
